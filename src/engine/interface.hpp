c207649cfb67b8d40c70fe1a45d4cf67ba226652
Sean Bikkes <bixkitts@gmail.com>
2023-01-08 20:17:25 +0100

    removed cluttered includes from interface

--- src/engine/interface.hpp
+++ src/engine/interface.hpp
@@ -1,10 +1,7 @@
-#ifndef FIRST_APP
-#define FIRST_APP
+#ifndef INTERFACE
+#define INTERFACE
 
 #include "bve_window.hpp"
-#include "bve_device.hpp"
-#include "bve_swap_chain.hpp"
-#include "bve_model.hpp"
 #include "bve_defines.hpp"
 
 namespace bve
